9ce9c040601adb382661a27631dd75d7cbc02aa1
Harsh Raghuvanshi <harshrghvnsh@gmail.com>
2024-05-29 12:45:23 +0530

    Added seperate learning files

---
+++ Learning.cpp
@@ -0,0 +1,5 @@
+// These are the important learning that  i have made through mistakes
+// 1. Whenever you are using Disjoint set union try to call the parent for each element once so that the actual parent of each node get set perfectly.
+// 2. In case of fenwick try always using 1 indexed array with it as it can create problem in case of zero
+// 3. In game theory question try to look at the problem in the form of a tree which is similiar to the three max min tree  which we have seen in AIES course work.
+// 4. While attempting oa or contest on leet code try to use your own min and max function as they are only capable of dealing with integers not with long long
