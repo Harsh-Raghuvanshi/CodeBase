e7ebc578e4656cdcf6b862a1b6de4fef651d9efc
Harsh Raghuvanshi <harshrghvnsh@gmail.com>
2025-01-01 19:48:08 +0530

    added sparse table and legandre's theorem

--- CODEBASE.cpp
+++ CODEBASE.cpp
@@ -847,6 +847,68 @@ void findHamiltonian(vector<vector<int>>&g,vector<int>&path,int vertex,vector<bo
 }
 
 
+//Legandre's theorem which is use to overcome the problem of :Given an integer n and a prime number p, the task is to find the largest x such that px (p raised to power x) divides n!.
+// Returns largest power of p that divides n! 
+int largestPower(int n, int p)  { 
+    int res = 0; 
+
+    // Calculate res = n/p + n/(p^2) + n/(p^3) + .... 
+    while (n > 0) { 
+        n /= p; 
+        res += n; 
+    } 
+    return res; 
+} 
+
+
+// Sparse Table
+// Fills lookup array 
+// lookup[][] in bottom up manner. [ this is min lookup array]
+void preprocess(vector<int>&arr, int n,vector<vector<int>>&lookup)
+{
+    // Initialize M for the 
+    // intervals with length 1
+    for (int i = 0; i < n; i++)
+        lookup[i][0] = i;
+ 
+    // Compute values from smaller 
+    // to bigger intervals
+    for (int j = 1; (1 << j) <= n; j++) 
+    {
+        // Compute minimum value for 
+        // all intervals with size
+        // 2^j
+        for (int i = 0; (i + (1 << j) - 1) < n; i++) 
+        {
+            // For arr[2][10], we 
+            // compare arr[lookup[0][3]]
+            // and arr[lookup[3][3]]
+            if (arr[lookup[i][j - 1]]
+                < arr[lookup[i + (1 << (j - 1))][j - 1]])
+                lookup[i][j] = lookup[i][j - 1];
+            else
+                lookup[i][j]
+                    = lookup[i + (1 << (j - 1))][j - 1];
+        }
+    }
+}
+ 
+// Returns minimum of arr[L..R]
+int query(vector<int>&arr, int L, int R,vector<vector<int>>&lookup)
+{
+    // For [2,10], j = 3
+    int j = (int)log2(R - L + 1);
+ 
+    // For [2,10], we compare arr[lookup[0][3]] and
+    // arr[lookup[3][3]],
+    if (arr[lookup[L][j]]
+        <= arr[lookup[R - (1 << j) + 1][j]])
+        return arr[lookup[L][j]];
+ 
+    else
+        return arr[lookup[R - (1 << j) + 1][j]];
+}
+ 
 int main()
 {
     return 0;
--- Learning.cpp
+++ Learning.cpp
@@ -6,8 +6,41 @@
 // 5. So whenver you declare a global variable let say count and try to use it in other functions then you will encounter an error like count is ambigious the main reason for that is under the namespace standard count is also present as function in header file which is causing ambiguity to resolve this issue do not use names which can be present in header files
 // 6. Did you know about the counting sort algorithm which can perform sorting in O(n) complexity if all number in the range 1e7
 // 7. See you need to just hash the given array and iterate over it and place the place in sorted order on the basis of the occurrence of the variables
-// 8. just like upper_bount and lower_bound function we have an equal_range function which returns the lowest index and highest index for the given value
+// 8. just like upper_bound and lower_bound function we have an equal_range function which returns the lowest index and highest index for the given value
 // in the same approach that lower_bound and upperbound use
+// 9. 
+// ⇒ Whenever we have to deal with range things which comes to mind is 
+// Hash array
+// Fenwick Tree
+// Segment Tree
+
+// ⇒ Now see Hash array is capable of producing the result in O(1) time for any range with O(n) precomputation.
+// But fails when it comes to update the element in between querying
+
+// ⇒ Here comes fenwick tree
+// Fenwick tree is capable of producing the result for a range in O(logn) by precomputation of O(nlogn) initially 
+// Also when it comes for updation[point update]  it can update in O(logn) time and produce results
+// But fails when we have to deal with range queries
+
+// ⇒ Also both of them are useless when you have to determine result of function where 
+// f(l to r) != f(r) - f(l-1)    like minimum in a range and maximum in a range
+
+// ⇒ But what if we have to deal with those function then we can use segment trees
+
+// ⇒ Here comes the Segment Tree which with the precomputation of O(nlogn) time is capable of answering range queries in O(logn) time
+
+// When it comes to Point update it is again take only O(logn) for updation
+// When it comes to Range update it take only O(logn) time again with the concept of lazy propagation and 
+
+// ⇒ Again there is a point let say we have function of type f(l to r) != f(r) - f(l-1) and query are static that there is no concept of changing the queries then we can use 
+
+// ⇒ Sparse Table
+// The are very similiar to binary lifting concept in binary tree where we construct a precompute array called lookup array of size n*logn such that if we have lookup[i][j] → then it means start from index i to 2^j -1 what is the result 
+
+// To query in sparse table we have to first calcuate the size of range which needed to be calculated then we can divide the range in to two parts which is needt to be calculated let say we have [L,R] then it can be ([L,L+ (1<<j)-1] =lookup[L][j]) + ([R-(1<<j)+1],R]=lookup[R-(1<<j)+1][j] remember they have overlapping portion also which is [R-(1<<j)+1,L+(1<<j)-1]
+
+
+
 
 #include<bits/stdc++.h>
 using namespace std;
